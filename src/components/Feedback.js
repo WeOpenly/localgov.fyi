86ea80b25f7d51c162358a59e3117c076789ede3
Jim Lee <jimleeisme@gmail.com>
2018-06-27 16:45:43 -0700

    Add feedback component

---
+++ src/components/Feedback.js
@@ -0,0 +1,199 @@
+import React, { Component, Fragment } from 'react';
+import Spinner from 'react-spinkit';
+
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import MoodBad from '@material-ui/icons/MoodBad';
+
+import withRoot from '../withRoot';
+
+const styles = theme => ({
+  wrapper: {
+    position: 'relative',
+  },
+  paper: {
+    position: 'absolute',
+    top: -244,
+    left: -230,
+    height: 266,
+    width: 332,
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    borderTop : `6px solid ${theme.palette.primary["500"]}`,
+  },
+  bootstrapInput: {
+    borderRadius: 3,
+    // backgroundColor: theme.palette.primary['50'],
+    color: theme.palette.primary['200'],
+    border: '1px solid #ced4da',
+    padding: '10px 12px 12px 12px',
+    marginTop: theme.spacing.unit,
+    width: '300px',
+    transition: theme.transitions.create(['border-color', 'box-shadow'])
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+  },
+  afterSubmit: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spinner: {
+    color: theme.palette.primary['500'],
+  },
+  icon: {
+    color: theme.palette.primary['500'],
+    fontSize: 32,
+    marginBottom: theme.spacing.unit * 2,
+  },
+});
+
+class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      email: '',
+      comment: '',
+      submitting: false,
+      success: false,
+      failure: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ open: !this.state.open });
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    let currentLoc = '';
+    if (window.location && window.location.pathname){
+        currentLoc = window.location.pathname
+    }
+
+    this.setState({ submitting: true });
+    fetch("/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: encode({
+        "form-name": "feedback",
+        "path": currentLoc,
+        "comment": this.state.comment,
+        "email": this.state.email,
+      })
+    }).then(() => this.setState({
+      submitting: false,
+      success: true,
+    })).catch(error => this.setState({
+      submitting: false,
+      failure: true,
+    }));
+
+    e.preventDefault();
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  handleReset() {
+    this.setState({
+      submitting: false,
+      success: false,
+      failure: false,
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const {
+      open,
+      comment,
+      email,
+      submitting,
+      success,
+      failure,
+    } = this.state;
+    return (
+      <div className={classes.wrapper}>
+        <Typography onClick={this.handleClick}>Send feedback</Typography>
+        {open && <Paper className={classes.paper}>
+          {(!success && !failure && !submitting) && <Fragment>
+            <Typography variant="display1">
+              Share your feedback
+            </Typography>
+            <form
+              name="feedback"
+              onSubmit={this.handleSubmit}
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <label>
+                <textarea
+                  name="comment"
+                  type="text"
+                  placeholder="Your comments"
+                  value={comment}
+                  onChange={this.handleChange}
+                  rows={4}
+                  className={classes.bootstrapInput}
+                />
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="your@email.com"
+                  value={email}
+                  onChange={this.handleChange}
+                  className={classes.bootstrapInput}
+                />
+              </label>
+              <Typography variant="caption">*We won't send you spam</Typography>
+              <Button variant="outlined" type="submit" className={classes.button}>
+                Submit
+              </Button>
+              <Button onClick={this.handleClose} className={classes.button}>Cancel</Button>
+            </form>
+          </Fragment>}
+          {submitting && <div className={classes.afterSubmit}>
+            <Spinner className={classes.spinner}/>
+          </div>}
+          {success && <div className={classes.afterSubmit}>
+            <SvgIcon className={classes.icon}>
+              <path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z"/>
+              <path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
+            </SvgIcon>
+            <Typography>Thanks for your feedback!</Typography>
+            <Button onClick={this.handleClose} className={classes.button}>Close</Button>
+          </div>}
+          {failure && <div className={classes.afterSubmit}>
+            <MoodBad className={classes.icon}/>
+            <Typography>Something went wrong. Please try again.</Typography>
+            <Button onClick={this.handleReset} className={classes.button}>Back</Button>
+          </div>}
+        </Paper>}
+      </div>
+    );
+  }
+}
+
+export default withRoot(withStyles(styles)(Feedback));
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+import Feedback from './Feedback';
 import withRoot from '../withRoot';
 
 const styles = theme => ({
   footer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
     paddingTop: theme.spacing.unit * 3,
     paddingBottom: theme.spacing.unit * 3,
-    paddingRight: theme.spacing.unit * 12,
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  copyright: {
+    marginRight: theme.spacing.unit,
+  },
+  item: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginRight: theme.spacing.unit,
+  },
+  privacy: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginLeft: '3rem',
+    marginRight: theme.spacing.unit,
   },
   link: {
     textDecoration: 'none',
-    marginLeft: theme.spacing.unit * 2,
   },
 });
 
@@ -24,17 +40,37 @@ class Footer extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <footer className={classes.footer}>
-        <Link to="/about/" className={classes.link}>
-          <Typography>About</Typography>
-        </Link>
-        <Link to="/terms/" className={classes.link}>
-          <Typography>Terms</Typography>
-        </Link>
-        <Link to="/privacy/" className={classes.link}>
-          <Typography>Privacy Policy</Typography>
-        </Link>
-      </footer>
+      <Grid container>
+        <Grid item xs={0} md={1} />
+        <Grid item xs={10} md={10}>
+          <footer className={classes.footer}>
+            <Grid container className={classes.container}>
+              <Grid item md={6} className={classes.copyright}>
+                <Typography>Copyright © 2018 Openly Technologies, Inc.</Typography>
+              </Grid>
+              <Grid item xs={12} md={2} className={classes.item}>
+                <Feedback />
+              </Grid>
+              <Grid item xs={12} md={1} className={classes.item}>
+                <Link to="/about/" className={classes.link}>
+                  <Typography>About</Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={12} md={1} className={classes.item}>
+                <Link to="/terms/" className={classes.link}>
+                  <Typography>Terms</Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={12} md={1} className={classes.privacy}>
+                <Link to="/privacy/" className={classes.link}>
+                  <Typography>Privacy Policy</Typography>
+                </Link>
+              </Grid>
+            </Grid>
+          </footer>
+        </Grid>
+        <Grid item xs={0} md={1} />
+      </Grid>
     );
   }
 }

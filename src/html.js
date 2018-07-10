87fb984a910e0982a04f32f31e20b213fe7983b6
ybv <ybv@batcave.local>
2018-07-09 18:14:16 -0700

    fixing css loading

--- src/html.js
+++ src/html.js
@@ -71,13 +71,13 @@ module.exports = class HTML extends React.Component {
 
           <script defer src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyC1d6ej2p77--6Wf8m6dzdrbvKhfBnb3Ks&libraries=places" type="text/javascript"></script>
 
-          {(process.env.NODE_ENV !== `production`) ? <link href={withPrefix('/css/fonts.css')} rel="stylesheet"/>  : null}
-          {(process.env.NODE_ENV !== `production`) ? <link href={withPrefix('/css/ionicons.min.css')} rel="stylesheet" /> : null}
+          {(process.env.NODE_ENV === `production`) ? <link href={withPrefix('/css/fonts.css')} rel="stylesheet"/>  : null}
+          {(process.env.NODE_ENV === `production`) ? <link href={withPrefix('/css/ionicons.min.css')} rel="stylesheet" /> : null}
 
           {this.props.headComponents}
           {css}
-          {fontcss}
-          {iconStyleCss}
+          {/* {fontcss}
+          {iconStyleCss} */}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}

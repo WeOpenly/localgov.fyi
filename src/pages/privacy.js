c83ba0f2b25fd36f13e02da1e933ab96ddf6ed3f
Jim Lee <jimleeisme@gmail.com>
2018-07-10 16:30:41 -0700

    Add privacy policy text

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -11,6 +11,15 @@ const styles = theme => ({
   container: {
     marginTop: theme.spacing.unit * 3,
   },
+  title: {
+    marginBottom: theme.spacing.unit * 3,
+  },
+  headline: {
+    marginBottom: theme.spacing.unit * 3,
+  },
+  body: {
+    marginBottom: theme.spacing.unit * 2,
+  },
 });
 
 class Privacy extends React.Component {
@@ -20,19 +29,23 @@ class Privacy extends React.Component {
       <Grid container className={classes.container}>
         <Grid item md={1}/>
         <Grid item md={10}>
-          <Typography align="left" variant="headline">
+          <Typography align="left" variant="headline" className={classes.title}>
             Privacy Policy
           </Typography>
         </Grid>
         <Grid item md={1}/>
         <Grid item md={1}/>
         <Grid item md={10}>
-          {[1,2,3,4,5].map(item => (
-            <div>
-            <Typography variant="subheading">{`${item}. Subheading`}</Typography>
-            <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-            </div>
-          ))}
+          <Typography variant="subheading" className={classes.headline}>What, how, and why:</Typography>
+          <Typography className={classes.body} gutterbottom>
+            We track user interactions such as, searches, suggestion clicks, page views, and external link clicks, browser fingerprint, type of browser, type of device, and ip address.
+          </Typography>
+          <Typography className={classes.body} gutterbottom>
+            We use these data points to understand anonymized user interactions with multiple components. This data helps us to understand searches that didn’t perform well, and to fine tune the user experience on a regular basis.
+          </Typography>
+          <Typography className={classes.body} gutterbottom>
+            All of the data we track is for internal purposes only, and we plan to keep it so.
+          </Typography>
         </Grid>
         <Grid item md={1}/>
       </Grid>

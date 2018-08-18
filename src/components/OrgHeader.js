26db1e451e785da868b8de18bbe1c82cceb97e74
Jim Lee <jimleeisme@gmail.com>
2018-08-18 15:31:05 -0700

    Styling changes

--- src/components/OrgHeader.js
+++ src/components/OrgHeader.js
@@ -15,7 +15,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import SvgIcon from '@material-ui/core/SvgIcon';
-import Share from '@material-ui/icons/Share';
 import withRoot from '../withRoot';
 
 const styles = theme => ({
@@ -28,7 +27,7 @@ const styles = theme => ({
     justifyContent: 'space-between',
     marginRight: -theme.spacing.unit,
   },
-  iconButton: {
+  menuButton: {
     marginTop: -theme.spacing.unit,
     marginRight: theme.spacing.unit * -1.5,
   },
@@ -44,6 +43,10 @@ const styles = theme => ({
   buttonContent: {
     display: 'flex',
     alignItems: 'center',
+  },
+  contactButton: {
+    marginLeft: -2,
+    marginRight: -2,
   },
 });
 
@@ -89,15 +92,15 @@ class OrgHeader extends Component {
     const sortInfo = (info) => {
       info.forEach((detail) => {
         let type = detail.contact_type;
-        if (type === 'ADDRESS') {
+        if (type === 'FACEBOOK') {
           sortedInfo[0] = detail;
-        } else if (type === 'PHONE') {
+        } else if (type === 'TWITTER') {
           sortedInfo[1] = detail;
         } else if (type === 'EMAIL') {
           sortedInfo[2] = detail;
-        } else if (type === 'FACEBOOK') {
+        } else if (type === 'PHONE') {
           sortedInfo[3] = detail;
-        } else if (type === 'TWITTER') {
+        } else if (type === 'ADDRESS') {
           sortedInfo[4] = detail;
         }
       })
@@ -167,9 +170,9 @@ class OrgHeader extends Component {
       }
 
       return (
-        <Button key={cd.contact_value}>
+        <IconButton key={cd.contact_value} className={classes.contactButton}>
           {value}
-        </Button>
+        </IconButton>
       );
     });
 
@@ -178,9 +181,9 @@ class OrgHeader extends Component {
         <CardContent>
           <div className={classes.cardTop}>
             <Typography variant="display1">{name}</Typography>
-            <IconButton onClick={this.handleShareClick} className={classes.iconButton}>
-              <Share/>
-            </IconButton>
+            <Button onClick={this.handleShareClick} className={classes.menuButton}>
+              Share
+            </Button>
             <Menu
               id="simple-menu"
               anchorEl={anchorEl}
@@ -189,21 +192,17 @@ class OrgHeader extends Component {
             >
               <MenuItem className={classes.menuItem}>
                 <CopyToClipboard text={shareLink} onCopy={this.handleCopy}>
-                  <Button variant="outlined">{copied ? 'Copied!' : 'Copy URL'}</Button>
+                  <Typography>{copied ? 'Copied!' : 'Copy URL'}</Typography>
                 </CopyToClipboard>
               </MenuItem>
               <MenuItem onClick={this.handleClose} className={classes.menuItem}>
                 <FacebookShareButton url={shareLink} className={classes.shareButton}>
-                  <SvgIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M426.8 64H85.2C73.5 64 64 73.5 64 85.2v341.6c0 11.7 9.5 21.2 21.2 21.2H256V296h-45.9v-56H256v-41.4c0-49.6 34.4-76.6 78.7-76.6 21.2 0 44 1.6 49.3 2.3v51.8h-35.3c-24.1 0-28.7 11.4-28.7 28.2V240h57.4l-7.5 56H320v152h106.8c11.7 0 21.2-9.5 21.2-21.2V85.2c0-11.7-9.5-21.2-21.2-21.2z"/>
-                  </SvgIcon>
+                  <Typography>Facebook</Typography>
                 </FacebookShareButton>
               </MenuItem>
               <MenuItem onClick={this.handleClose} className={classes.menuItem}>
                 <TwitterShareButton url={shareLink} className={classes.shareButton}>
-                  <SvgIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M492 109.5c-17.4 7.7-36 12.9-55.6 15.3 20-12 35.4-31 42.6-53.6-18.7 11.1-39.4 19.2-61.5 23.5C399.8 75.8 374.6 64 346.8 64c-53.5 0-96.8 43.4-96.8 96.9 0 7.6.8 15 2.5 22.1-80.5-4-151.9-42.6-199.6-101.3-8.3 14.3-13.1 31-13.1 48.7 0 33.6 17.2 63.3 43.2 80.7-16-.4-31-4.8-44-12.1v1.2c0 47 33.4 86.1 77.7 95-8.1 2.2-16.7 3.4-25.5 3.4-6.2 0-12.3-.6-18.2-1.8 12.3 38.5 48.1 66.5 90.5 67.3-33.1 26-74.9 41.5-120.3 41.5-7.8 0-15.5-.5-23.1-1.4C62.8 432 113.7 448 168.3 448 346.6 448 444 300.3 444 172.2c0-4.2-.1-8.4-.3-12.5C462.6 146 479 129 492 109.5z"/>
-                  </SvgIcon>
+                  <Typography>Twitter</Typography>
                 </TwitterShareButton>
               </MenuItem>
             </Menu>

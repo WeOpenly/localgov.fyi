b763f0b25493b9ff428cf80a5fe3bfb7f8a8439c
Jim Lee <jimleeisme@gmail.com>
2018-08-20 19:10:53 -0700

    Feedback popup on service link click

--- src/components/ServiceDeliveryLink.js
+++ src/components/ServiceDeliveryLink.js
@@ -5,10 +5,15 @@ import { connect } from "react-redux";
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import MoodBad from '@material-ui/icons/MoodBad';
 import Spinner from 'react-spinkit';
 
 import withRoot from '../withRoot';
-import { trackClick } from "./Search/tracking";
+import { trackClick, trackInput } from "./Search/tracking";
 
 const styles = theme => ({
   root: {
@@ -38,40 +43,155 @@ const styles = theme => ({
     alignItems: 'center',
   },
   button: {
-    marginBottom: theme.spacing.unit
-  }
+    marginBottom: theme.spacing.unit,
+  },
+  paper: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit * 2,
+    borderTop : `6px solid ${theme.palette.primary["500"]}`,
+  },
+  form: {
+    width: 300,
+  },
+  bootstrapInput: {
+    borderRadius: 3,
+    color: theme.palette.primary['200'],
+    border: '1px solid #ced4da',
+    padding: '10px 12px 12px 12px',
+    marginTop: theme.spacing.unit,
+    width: '300px',
+    transition: theme.transitions.create(['border-color', 'box-shadow'])
+  },
+  bootstrapInputComment:{
+    borderRadius: 3,
+    color: theme.palette.primary['200'],
+    border: '1px solid #ced4da',
+    padding: '10px 12px 12px 12px',
+    marginTop: theme.spacing.unit,
+    width: '300px',
+    height: '100px',
+    'wordBreak': 'break-word',
+    transition: theme.transitions.create(['border-color', 'box-shadow'])
+  },
+  dialogButton: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit,
+  },
+  afterSubmit: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spinner: {
+    color: theme.palette.primary['500'],
+  },
+  icon: {
+    color: theme.palette.primary['500'],
+    fontSize: 32,
+    marginBottom: theme.spacing.unit * 2,
+  },
 });
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 class ServiceDeliveryLink extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      'redirectClicked': false
-    }
-    this.onClick = this
-      .onClick
-      .bind(this);
+      redirectClicked: false,
+      feedbackOpen: false,
+      email: '',
+      feedbackComment: '',
+      submitting: false,
+      success: false,
+      failure: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
-  onClick(name, url, index) {
+  handleClick(name, url, index) {
     const {trackClick} = this.props;
     const windowGlobal = typeof window !== 'undefined' && window
     trackClick('external', 'service_delivery_link', url, name, index);
     this.setState({
-      'redirectClicked': true
+      redirectClicked: true,
     }, () => {
       setTimeout(() => windowGlobal.open(url) , 1000); 
-setTimeout(() => {
-  this.setState({
-    redirectClicked: false
-  })
-}, 5000);
-    })
+      setTimeout(() => {
+        this.setState({ redirectClicked: false, feedbackOpen: true, })
+      }, 2000);
+    });
+  }
 
+  handleClose() {
+    this.setState({ feedbackOpen: false });
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    const { trackFeedback} = this.props;
+    let currentLoc = '';
+    if (window.location && window.location.pathname){
+        currentLoc = window.location.pathname
+    }
+    e.preventDefault();
+
+    this.setState({ submitting: true });
+    fetch("/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: encode({
+        "form-name": "feedback",
+        "path": currentLoc,
+        "feedbackComment": this.state.feedbackComment,
+        "email": this.state.email,
+      })
+    }).then(() => this.setState({
+      submitting: false,
+      success: true,
+    })).catch(error => this.setState({
+      submitting: false,
+      failure: true,
+    }));
+
+    trackFeedback();
+  }
+
+  handleReset() {
+    this.setState({
+      submitting: false,
+      success: false,
+      failure: false,
+    });
   }
 
   render() {
     const {classes, serDelLinks, org_name, service_name} = this.props;
+    const {
+      feedbackOpen,
+      feedbackComment,
+      email,
+      submitting,
+      success,
+      failure,
+    } = this.state;
     if (!serDelLinks) {
       return null;
     }
@@ -84,7 +204,7 @@ setTimeout(() => {
       return (
         <Button
           key={link.link_name}
-          onClick={() => this.onClick(link.link_name, link.url, idx)}
+          onClick={() => this.handleClick(link.link_name, link.url, idx)}
           variant="raised"
           color="primary"
           className={classes.button}>
@@ -103,6 +223,83 @@ setTimeout(() => {
             </Typography>
           )
           : null}
+        <Dialog
+          open={feedbackOpen}
+          onClose={this.handleClose}
+        >
+          <Paper className={classes.paper}>
+            {(!success && !failure && !submitting) && <Fragment>
+              <Typography variant="title" component="h1">
+                How are we doing?
+              </Typography>
+              <form
+                name="feedback"
+                method="post"
+                action="/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+                className={classes.form}
+              >
+                <input type="hidden" name="form-name" value="feedback" />
+                <p hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </p>
+                <p hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="path" type="text" value=""/>
+                  </label>
+                </p>
+                <label>
+                  <textarea
+                    name="feedbackComment"
+                    type="text"
+                    placeholder="Your comments"
+                    value={feedbackComment}
+                    onChange={this.handleChange}
+                    rows={4}
+                    className={classes.bootstrapInput}
+                  />
+                </label>
+                <label>
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="your@email.com"
+                    value={email}
+                    onChange={this.handleChange}
+                    className={classes.bootstrapInput}
+                  />
+                </label>
+                <Typography variant="caption">*We won't send you spam</Typography>
+                <Button size="small" variant="outlined" type="submit" className={classes.dialogButton}>
+                  Submit
+                </Button>
+                <Button size="small" onClick={this.handleClose} className={classes.dialogButton}>Cancel</Button>
+              </form>
+            </Fragment>}
+            {submitting && <div className={classes.afterSubmit}>
+              <Spinner className={classes.spinner}/>
+            </div>}
+            {success && <div className={classes.afterSubmit}>
+              <SvgIcon className={classes.icon}>
+                <path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z"/>
+                <path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
+              </SvgIcon>
+              <Typography variant="body1" component="h1">Thanks for your feedback!</Typography>
+              <Button size="small" variant="outlined" onClick={this.handleClose} className={classes.dialogButton}>Close</Button>
+            </div>}
+            {failure && <div className={classes.afterSubmit}>
+              <MoodBad className={classes.icon}/>
+              <Typography variant="body1" >Something went wrong. Please try again.</Typography>
+              <Button size="small" variant="outlined" onClick={this.handleReset} className={classes.dialogButton}>Back</Button>
+            </div>}
+          </Paper>
+        </Dialog>
       </Fragment>
     );
   }
@@ -112,7 +309,10 @@ const mapDispatchToProps = (dispatch) => {
   return {
     trackClick: (click_type, resultType, id, title, listIndex) => {
       dispatch(trackClick(click_type, resultType, id, title, listIndex));
-    }
+    },
+    trackFeedback: () => {
+      dispatch(trackInput('feedback_form', ''));
+    },
   }
 }
 

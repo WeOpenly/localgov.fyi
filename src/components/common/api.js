6b3e96bb5ef02977469c4eb0ca971d99bcba8168
ybv <ybv@batcave.local>
2019-01-03 14:05:22 -0800

    flip yusuf backend

--- src/components/common/api.js
+++ src/components/common/api.js
@@ -37,7 +37,7 @@ export function getCookie(name) {
 
 // const BACKEND = 'http://127.0.0.1:8090/dashboard/api';
 // console.log(BACKEND);
-const YUSUF_BACKEND = `https://yusuf.localgov.fyi/api`
+const YUSUF_BACKEND = `https://localgov.fyi/api/yusuf`
 
 export function YusufApi(lang = undefined, endPoint, headers = {}) {
 

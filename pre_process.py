a93d12c8a2f9d6b7fb244edb61596a616f8301d8
ybv <ybv@batcave.local>
2019-01-14 12:08:31 -0800

    uncomment

--- pre_process.py
+++ pre_process.py
@@ -22,89 +22,89 @@ downloaded_orgs = []
 # download the logo with a new name (<id> _org_log)
 
 
-# for root, dirs, files in os.walk(data_dir):
-#     for filename in files:
-#         if not filename.endswith(".json"):
-#             continue
+for root, dirs, files in os.walk(data_dir):
+    for filename in files:
+        if not filename.endswith(".json"):
+            continue
         
-#         full_path = '{d}{f}'.format(d=data_dir, f=filename)
-#         rewrite = False
-#         with open(full_path) as f:
-#             # time = os.path.getmtime(full_path)
-#             # if datetime.fromtimestamp(time).date() < datetime.today().date():
-#             #     removable_files.append(full_path)
+        full_path = '{d}{f}'.format(d=data_dir, f=filename)
+        rewrite = False
+        with open(full_path) as f:
+            # time = os.path.getmtime(full_path)
+            # if datetime.fromtimestamp(time).date() < datetime.today().date():
+            #     removable_files.append(full_path)
             
-#             data = json.load(f)
-#             is_success = True if ('success' in data) and (data['success'] == True) else False
-#             if not is_success:
-#                 removable_files.append(full_path)
-#                 continue
+            data = json.load(f)
+            is_success = True if ('success' in data) and (data['success'] == True) else False
+            if not is_success:
+                removable_files.append(full_path)
+                continue
             
-#             details = data['details']
-#             org_id = details.get('id')
+            details = data['details']
+            org_id = details.get('id')
 
-#             has_services = True if len(details.get('services', [])) > 0 else False
-#             has_cd = True if len(details.get('contact_details', [])) > 0 else False
-#             org_logo = details.get('logo_url')
+            has_services = True if len(details.get('services', [])) > 0 else False
+            has_cd = True if len(details.get('contact_details', [])) > 0 else False
+            org_logo = details.get('logo_url')
 
-#             if org_logo:
-#                 file_name, file_ext = os.path.splitext(org_logo)
-#                 org_logo_filename = u"{l}{id}_org_logo{e}".format(l=logo_dir, id=org_id, e=file_ext)
-#                 if not os.path.exists(org_logo_filename):
-#                     with open(org_logo_filename, 'w+'):
-#                         pass
-#                 urllib.urlretrieve(org_logo, org_logo_filename)
+            if org_logo:
+                file_name, file_ext = os.path.splitext(org_logo)
+                org_logo_filename = u"{l}{id}_org_logo{e}".format(l=logo_dir, id=org_id, e=file_ext)
+                if not os.path.exists(org_logo_filename):
+                    with open(org_logo_filename, 'w+'):
+                        pass
+                urllib.urlretrieve(org_logo, org_logo_filename)
 
             
-#             for service in details.get('services', []):
-#                 services_in_detail = service.get('services', [])
-#                 for service_detail in services_in_detail:
-#                     ser_id = service_detail.get('id')
-#                     service_logo = service_detail.get('logo_url')
-#                     service_detail['service_flow_steps'] = []
-#                     rewrite = True
+            for service in details.get('services', []):
+                services_in_detail = service.get('services', [])
+                for service_detail in services_in_detail:
+                    ser_id = service_detail.get('id')
+                    service_logo = service_detail.get('logo_url')
+                    service_detail['service_flow_steps'] = []
+                    rewrite = True
                     
-#                     if not service_detail.get('delivery_enabled', False):
-#                         service_detail.update({
-#                             'delivery_enabled': False
-#                         })
-#                         rewrite = True
+                    if not service_detail.get('delivery_enabled', False):
+                        service_detail.update({
+                            'delivery_enabled': False
+                        })
+                        rewrite = True
 
-#                     # if service_detail.get('service_flow_steps'):
-#                     #     service_detail.update({
-#                     #         'service_flow_steps': []
-#                     #     })
-#                     #     rewrite = True
+                    # if service_detail.get('service_flow_steps'):
+                    #     service_detail.update({
+                    #         'service_flow_steps': []
+                    #     })
+                    #     rewrite = True
                     
-#                     if not service_logo:
-#                         continue
+                    if not service_logo:
+                        continue
 
-#                     # file_name, file_ext = os.path.splitext(service_logo)
-#                     # ser_log_filename = u"{l}{id}_ser_logo{e}".format(
-#                     #     l=logo_dir, id=ser_id, e=file_ext)
+                    # file_name, file_ext = os.path.splitext(service_logo)
+                    # ser_log_filename = u"{l}{id}_ser_logo{e}".format(
+                    #     l=logo_dir, id=ser_id, e=file_ext)
                     
-#                     # if not os.path.exists(ser_log_filename):
-#                     #     with open(ser_log_filename, 'w+'):
-#                     #         pass
-#                     # urllib.urlretrieve(service_logo, ser_log_filename)
+                    # if not os.path.exists(ser_log_filename):
+                    #     with open(ser_log_filename, 'w+'):
+                    #         pass
+                    # urllib.urlretrieve(service_logo, ser_log_filename)
 
-#             if has_services and not has_cd:
-#                 no_cd_files.append(full_path)
+            if has_services and not has_cd:
+                no_cd_files.append(full_path)
             
-#             if not all([has_services, has_cd]):
-#                 removable_files.append(full_path)
+            if not all([has_services, has_cd]):
+                removable_files.append(full_path)
         
-#         print rewrite
-#         if rewrite:
-#             with open(full_path, 'w') as the_file:
-#                 the_file.write(json.dumps(data))
+        print rewrite
+        if rewrite:
+            with open(full_path, 'w') as the_file:
+                the_file.write(json.dumps(data))
 
 
 
-# fncd = open("no_cd.txt", "w")
-# fncd.write(str(no_cd_files))
-# fnser = open("no_cd_ser.txt", "w")
-# fnser.write(str(removable_files))
+fncd = open("no_cd.txt", "w")
+fncd.write(str(no_cd_files))
+fnser = open("no_cd_ser.txt", "w")
+fnser.write(str(removable_files))
 
 for rem in removable_files:
     try:
@@ -135,5 +135,5 @@ def get_all_locations():
             with open(all_orgs_file, 'w+') as file_to_write:
                 json.dump(orgs, file_to_write)
     
-#get_service_glossary_items()
+get_service_glossary_items()
 get_all_locations()

f1b5220ce1d92bfa89a8079bdce272cf516a90b2
ybv <ybv@batcave.local>
2019-02-10 20:05:08 -0800

    change msg

--- src/components/Delivery/StepDetailFactory.js
+++ src/components/Delivery/StepDetailFactory.js
@@ -107,7 +107,7 @@ class StepDetailFactory extends React.Component {
                     <path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z" />
                     <path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" />
                 </SvgIcon>
-                <Typography variant="body1" component="h1">Your Request is authorized, we will reach out to your soon!</Typography>
+                <Typography variant="body1" component="h1">Your Request is authorized, we will reach out to you soon!</Typography>
                </div>);
 
         switch (step.type) {

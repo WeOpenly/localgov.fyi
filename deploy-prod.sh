ef2467010c8e05559dafcb3f0d3e6119026ebd57
ybv <ybv@batcave.local>
2019-02-27 22:15:42 -0800

    changes

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,3 +1,1 @@
-curl - H "Content-Type: application/zip" \
-     - H "Authorization: Bearer ${NETLIFY_AUTH_TOKEN}" \
-    --data - binary "@website.zip"  "https://api.netlify.com/${NETLIFY_SITE_ID}api/v1/sites"
+curl - H "Content-Type: application/zip" - H "Authorization: Bearer ${NETLIFY_AUTH_TOKEN}" --data - binary "@website.zip"  "https://api.netlify.com/${NETLIFY_SITE_ID}api/v1/sites"

216dbbb02c9ca5d7abf80610b40584c95f8bd277
ybv <ybv@batcave.local>
2019-09-05 16:26:31 -0700

    changes

--- packages/one/src/components/Services/serviceReducer.js
+++ packages/one/src/components/Services/serviceReducer.js
@@ -9,10 +9,21 @@ const initialState = {
         id: 0,
         name: "Property Taxes",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description:
+              "Property taxes fall into two categories, Secured and Unsecured. A Secured property tax is a tax for a physical property, most often real estate and land. An Unsecured property tax is for personal items, like an airplane, boat, or business. "
+          },
+          {
+            header: "How often do I need to do this?",
+            description: "Property taxes payments depend on the county or the school district the property is located in. It is usually paid in two parts every year."
+          }
+        ],
         initialFormData: {
-            "adddress": null,
-            "parcelId": null,
-            "billFile": [],
+          adddress: null,
+          parcelId: null,
+          billFile: []
         },
         formSchema: [
           {
@@ -64,6 +75,16 @@ const initialState = {
         id: 1,
         name: "Utility Bill",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description: "it's a bill"
+          },
+          {
+            header: "How often do I need to do this?",
+            description: "Once a month, usually"
+          }
+        ],
         formSchema: [
           {
             id: "providerName",
@@ -121,6 +142,16 @@ const initialState = {
         id: 2,
         name: "Pet Licence",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description: "it's a licence"
+          },
+          {
+            header: "How often do I need to do this?",
+            description: "Once a year, usually"
+          }
+        ],
         formSchema: [
           {
             id: "licenceNumber",
@@ -154,7 +185,17 @@ const initialState = {
       },
       {
         id: 3,
-        name: "Vehicle Registration",
+        name: "Renew Vehicle Registration",
+        faqs: [
+          {
+            header: "What is this about?",
+            description: "it's a renewal"
+          },
+          {
+            header: "How often do I need to do this?",
+            description: "Most vehicle registrations are valid only for an year and need to be renewed annually."
+          }
+        ],
         icon: null,
         formSchema: [
           {
@@ -200,6 +241,18 @@ const initialState = {
         id: 0,
         name: "Renew Business Licence",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description:
+              "A business license permits a business to operate in a specific town, city, or state. Almost all business require to have them filed."
+          },
+          {
+            header: "How often do I need to do this?",
+            description:
+              "City Business licenses typically require annual renewal. "
+          }
+        ],
         formSchema: [
           {
             id: "busName",
@@ -288,6 +341,18 @@ const initialState = {
         id: 1,
         name: "Renew Fictitious Business Licence or DBA",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description:
+              'A fictitious business name, sometimes referred as DBA, short for "doing business as," filing for a DBA allows you to conduct business under a name other than your own—your DBA is different from your name as the business owner, or your business\'s legal, registered name.'
+          },
+          {
+            header: "How often do I need to do this?",
+            description:
+              "DBA filing deadlines vary depending on the county you are operating your business in."
+          }
+        ],
         formSchema: [
           {
             id: "licenceFile",
@@ -367,13 +432,24 @@ const initialState = {
         id: 3,
         name: "Delaware Franchise Tax Filing",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description:
+              "All active Domestic Corporation Annual Reports and Franchise Taxes for the prior year are due annually."
+          },
+          {
+            header: "How often do I need to do this?",
+            description:
+              "Delaware Franchise Taxes for the prior year should be paid on or before March 1st every year."
+          }
+        ],
         formSchema: [
           {
             id: "licenceFile",
             name: "licenceFile",
             label: "Upload your Certificate of Incorporation",
-            description:
-              "Upload their Certificate of Incorporation",
+            description: "Upload their Certificate of Incorporation",
             type: "fileArray",
             validationType: "mixed",
             validations: [
@@ -389,6 +465,18 @@ const initialState = {
         id: 4,
         name: "California Franchise Tax Filing",
         icon: null,
+        faqs: [
+          {
+            header: "What is this about?",
+            description:
+              "All new limited liability companies (LLCs), S corporations, C corporations, limited partnerships (LPs), and limited liability partnershps (LLPs) registered in the state of California should pay a franchise tax. "
+          },
+          {
+            header: "How often do I need to do this?",
+            description:
+              "California Franchise Tax must be paid during the first quarter of each accounting period for your business. This is usually the first quarter of the calendar year for most companies."
+          }
+        ],
         formSchema: [
           {
             id: "corpNumber",
@@ -419,7 +507,7 @@ const initialState = {
             ]
           }
         ]
-      },
+      }
     ]
   },
   stripeCardModalOpen: false,

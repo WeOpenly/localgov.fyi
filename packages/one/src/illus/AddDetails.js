f8438d49fba330c4b053311c9d3c1506b573043f
ybv <ybv@batcave.local>
2019-09-06 16:15:38 -0700

    changes

---
+++ packages/one/src/illus/AddDetails.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+import Img from "gatsby-image";
+
+const AddDetails = () => (
+  <StaticQuery
+    query={graphql`
+      query AddDetailsq {
+        heroIl: allFile(filter: { relativePath: { eq: "STEP2.png" } }) {
+          edges {
+            node {
+              name
+              childImageSharp {
+                fluid {
+                  base64
+                  tracedSVG
+                  aspectRatio
+                  src
+                  srcSet
+                  srcWebp
+                  srcSetWebp
+                  sizes
+                  originalImg
+                  originalName
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      return (
+        <Img
+          title={`evergov`}
+          alt={`Add details`}
+          style={{ width: "180px" }}
+          sizes={data.heroIl.edges[0].node.childImageSharp.fluid}
+        />
+      );
+    }}
+  />
+);
+
+export default AddDetails;

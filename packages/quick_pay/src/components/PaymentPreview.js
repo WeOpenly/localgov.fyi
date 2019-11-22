271e47d7c4b020ee89e3357836b523edcecc08ff
ybv <ybv@batcave.local>
2019-11-21 18:39:39 -0700

    changes

--- packages/quick_pay/src/components/PaymentPreview.js
+++ packages/quick_pay/src/components/PaymentPreview.js
@@ -149,7 +149,7 @@ class PaymentPreview extends Component {
                         style={{
                           display: "flex",
                           justifyContent: "center",
-                          padding: "8px 8px",
+                          padding: "8px 8px"
                         }}
                       >
                         <div
@@ -170,13 +170,128 @@ class PaymentPreview extends Component {
                             }}
                             className={`${iconStyles.typcn} ${iconStyles.typcnWarning}`}
                           ></span>
-                          Payment was not successfull because {" "}
+                          Payment was not successfull because{" "}
                           {this.state.failedMsg}
                         </div>
                       </li>
                     ) : null}
-                   
                   </ul>
+                  <li
+                    className={styles.menuItem}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      padding: "16px 8px"
+                    }}
+                  >
+                    <h5>Payment Summary</h5>
+                  </li>
+                  <li
+                    className={styles.menuItem}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between"
+                    }}
+                  >
+                    <div>
+                      <span
+                        className={`${iconStyles.typcn} ${iconStyles.typcnCreditCard}`}
+                      ></span>
+                      Amount to pay
+                    </div>
+
+                    <div className={styles.menuItem}>
+                      {`$`}
+                      {price}
+                    </div>
+                  </li>
+                  <li className={styles.divider}></li>
+                  <li
+                    className={styles.menuItem}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between"
+                    }}
+                  >
+                    <div
+                      className={`${styles.tooltip} ${styles.tooltipRight}`}
+                      data-tooltip="papergov performs this on your behalf for a small fee"
+                    >
+                      <span
+                        className={`${iconStyles.typcn} ${iconStyles.typcnSupport}`}
+                      ></span>
+                      Service Fee
+                      <sup>
+                        <span
+                          className={`${iconStyles.typcn} ${iconStyles.typcnInfoLarge}`}
+                        ></span>
+                      </sup>
+                    </div>
+
+                    <div className={styles.menuItem}>5%</div>
+                  </li>
+                  <li className={styles.divider}></li>
+
+                  <li
+                    className={styles.menuItem}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      padding: "8px 10px"
+                    }}
+                  >
+                    <div style={{ fontWeight: "bold" }}>Total</div>
+
+                    <div
+                      className={styles.menuItem}
+                      style={{ fontWeight: "bold" }}
+                    >
+                      {"$"}
+                      {finalAmtShow}
+                    </div>
+                  </li>
+
+                  <li
+                    className={styles.menuItem}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      padding: "8px 8px 0 8px"
+                    }}
+                  >
+                    <button
+                      style={{
+                        marginTop: "16px",
+                        width: "100%",
+                        fontSize: "14px"
+                      }}
+                      className={`${styles.btn} ${styles.btnPrimary} ${styles.btnLg} ${styles.textUppercase} ${styles.textBold}`}
+                      type="submit"
+                    >
+                      {" "}
+                      <span
+                        className={`${iconStyles.typcn} ${iconStyles.typcnThumbsUp}`}
+                      ></span>
+                      Pay {"$"}
+                      {finalAmtShow}
+                    </button>
+                  </li>
+                  <li
+                    className={styles.menuItem}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      padding: "8px 8px",
+                      alignItems: "center"
+                    }}
+                  >
+                    <small style={{ fontSize: "12px" }}>
+                      <span
+                        className={`${iconStyles.typcn} ${iconStyles.typcnInfoLarge}`}
+                      ></span>
+                      You accept terms by cicking this
+                    </small>
+                  </li>
                 </form>
               </div>
             </div>

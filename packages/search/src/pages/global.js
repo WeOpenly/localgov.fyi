96162b770a42fc38e0263ce452f28c112e5f0dec
Vaibhav Krishna <krish@weopenly.com>
2020-07-01 12:24:59 +0200

    Update global.js

--- packages/search/src/pages/global.js
+++ packages/search/src/pages/global.js
@@ -23,7 +23,39 @@ import styles from "../components/spectre.min.module.css";
 import iconStyles from "../components/typicons.min.module.css";
 
 
-class Index extends React.Component {
+import { navigate } from 'gatsby-link'
+import Layout from '../layout'
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export default function Contact() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+
+class GIndex extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -84,7 +116,56 @@ class Index extends React.Component {
                 appReady={appReady}
                 location={this.props.location}
               />
-            </div>           
+            </div>      
+           <div>
+             return (
+    <Layout>
+      <h1>Contact</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Layout>
+  )
+}
+
+           </div>
     
           </div>
 
@@ -103,6 +184,6 @@ const mapStateToProps = function (state, ownProps) {
   };
 };
 
-const ConnIndex = connect(mapStateToProps)(Index);
+const GlobalIndex = connect(mapStateToProps)(Index);
 
-export default ConnIndex;
+export default GlobalIndex;

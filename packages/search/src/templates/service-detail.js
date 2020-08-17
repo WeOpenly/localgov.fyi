3dc291a640f6c408337c285910c589812ed0c417
Vaibhav Krishna <krish@weopenly.com>
2020-08-17 09:19:04 +0200

    Update service-detail.js

--- packages/search/src/templates/service-detail.js
+++ packages/search/src/templates/service-detail.js
@@ -484,18 +484,7 @@ class ServiceDetailTemplate extends React.Component {
                 </div>
 
                 <div className={`${styles.column} ${styles.col12}`}>
-                  <h3 style={{ margin: "2rem 0" }}>Community Q&A</h3>
-                  <p> You can ask any questions related to this service here. We will try to answer them soon</p>
-                   <DiscussionEmbed
-                     shortname='papergov'
-                     config={
-                       {
-                         url: this.props.url_slug,
-                         identifier: this.props.id
-                       }
-                     }
-                   />
-                  <br></br>
+                  <CommunityQuestion />
                 </div>
 
 
@@ -604,9 +593,11 @@ class ServiceDetailTemplate extends React.Component {
                     allSteps={allSteps}
                   />
                 </div>
-                 <div className={`${styles.column} ${styles.col12}`}> 
-                  <h3 style={{ margin: "2rem 0" }}>Community Forum</h3>
-                  <p> You can ask any questions related to this service here. We will try to answer them soon</p>
+
+                <div className={`${styles.column} ${styles.col12}`}> 
+                 <h3 style={{ margin: "2rem 0" }}>Community Forum </h3>
+                  <p> You can ask any questions related to this service here. We will try to answer them soon! </p>
+                   
                    <DiscussionEmbed
                      shortname='papergov'
                      config={
@@ -618,6 +609,7 @@ class ServiceDetailTemplate extends React.Component {
                    />
                   <br></br>
                  </div>
+                
                 <div className={`${styles.column} ${styles.col12}`}>
                   <MoreLinks
                     isMobile={isMobile}

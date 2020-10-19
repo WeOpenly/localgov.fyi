42c57aa996e3daf1997fda8f0f732ce030b1b08c
Vaibhav Krishna <krish@weopenly.com>
2020-10-19 15:16:38 +0200

    Update deploy.sh

--- packages/search/deploy.sh
+++ packages/search/deploy.sh
@@ -1,1 +1,1 @@
-./node_modules/.bin/netlify deploy --site $NETLIFY_SITE --auth $NETLIFY_AUTH_TOKEN --dir public --prod --timeout 36000000
+./node_modules/.bin/netlify deploy --site $NETLIFY_SITE --auth $NETLIFY_AUTH_TOKEN --dir public --prod --timeout 3600000

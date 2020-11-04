de7113013144961c688052c7fc77fcf6ed5f99cd
Vaibhav Krishna <krish@weopenly.com>
2020-11-04 17:02:23 +0100

    Update deploy.sh

--- packages/search/deploy.sh
+++ packages/search/deploy.sh
@@ -1,2 +1,2 @@
-NODE_OPTIONS = "--max_old_space_size=4096" DEBUG=* ./node_modules/.bin/netlify deploy --site $NETLIFY_SITE --auth $NETLIFY_AUTH_TOKEN --dir public --prod --timeout 24000 
+NODE_OPTIONS = --max_old_space_size=4096 DEBUG=* ./node_modules/.bin/netlify deploy --site $NETLIFY_SITE --auth $NETLIFY_AUTH_TOKEN --dir public --prod --timeout 24000 
 
